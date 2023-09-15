bbceaadc7f712d3bd22016c783647f9934ea548b
BigGlaucos <glaucoscc@gmail.com>
2023-09-15 10:07:50 -0300

    add //

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,17 @@
-const Sequilize = require("sequelize");
+// Importando a biblioteca Sequelize
+const Sequelize = require("sequelize");
 
+// Importando as configurações do banco de dados do arquivo ../config/database.js
 const dbConfig = require("../config/database");
 
+// Importando o modelo User definido no arquivo ../models/user.js
 const User = require("../models/user");
 
-const connection = new Sequilize(dbConfig);
+// Criando uma nova instância de conexão com o banco de dados usando as configurações
+const connection = new Sequelize(dbConfig);
 
+// Inicializando o modelo User com a conexão ao banco de dados
 User.init(connection);
 
+// Exportando a conexão configurada com o banco de dados
 module.exports = connection;
