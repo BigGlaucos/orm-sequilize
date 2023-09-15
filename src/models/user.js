1bb23d23677a9f439ac8b19c8b4a99dc3e898715
BigGlaucos <glaucoscc@gmail.com>
2023-09-15 09:52:57 -0300

    fix

--- src/models/user.js
+++ src/models/user.js
@@ -4,8 +4,8 @@ class User extends Model {
     static init(connection) {
         super.init(
             {
-                name: Datatypes.String,
-                email: Datatypes,
+                name: Datatypes.STRING,
+                email: Datatypes.STRING,
             },
             {
                 sequelize: connection,
